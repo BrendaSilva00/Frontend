6c9a1462b6269d2ec9b30ef3da6e698f7a284281
BrendaSilva00 <116496244+BrendaSilva00@users.noreply.github.com>
2023-04-02 21:22:20 -0300

    backend

---
+++ Backend/mpw/src/main/java/com/miportfolio/mpw/MpwApplication.java
@@ -0,0 +1,13 @@
+package com.miportfolio.mpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpwApplication.class, args);
+	}
+
+}
