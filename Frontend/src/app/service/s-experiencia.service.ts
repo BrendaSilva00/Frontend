e74abcaec37701d8da725275b6efd2c47b281d2b
BrendaSilva00 <116496244+BrendaSilva00@users.noreply.github.com>
2023-04-16 20:48:37 -0300

    f

---
+++ Frontend/src/app/service/s-experiencia.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SExperienciaService {
+  expURL = 'http://localhost:8080/explab/'
+
+  constructor() { }
+}
